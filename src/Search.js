4137a8768d309656476cc5cfb972cf24e481a014
Hamaddogar <hamadali77115033@gmail.com>
2023-02-01 11:34:37 +0500

    error solved search-working

--- src/Search.js
+++ src/Search.js
@@ -1,55 +1,56 @@
 import { check } from "prettier";
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import SearchButton from "./SearchButton";
 
 const SearchBar = (props) => {
-
   const [searchInput, setSearchInput] = useState("");
- 
 
- 
- const handleChange = (e) => {
-   e.preventDefault();
-   setSearchInput(e.target.value);
- };
- 
- if (searchInput.length > 0) {
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
 
-      props.alldata.filter((firstName) => {
-     return firstName.name.match(searchInput);
- });
- }
- 
- return <div>
- 
- {console.log(props.alldata)}
- <input
-    type="search"
-    placeholder="Search here"
-    onChange={handleChange}
-    value={searchInput} />
- 
- <table>
-   <tr>
-     <th>firstName</th>
-     <th>surname</th>
-   </tr>
- 
- {/* {customers.map((firstName, *index*) => {
- 
- <div>
-   <tr>
-     <td>{firstName.name}</td>
-     
-   </tr>
- </div>
- 
- })} */}
- </table>
- 
- </div>
- 
- 
- };
- 
- export default SearchBar;
+  const filteredData = props.alldata.filter((item) =>
+    item.firstName.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
+  return (
+    <div className="container">
+      <input
+        type="search"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={searchInput}
+      />
+
+      <table class="table table-bordered">
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>First Name</th>
+          <th>Sur Name</th>
+          <th>Email</th>
+          <th>Room ID</th>
+          <th>Check In Date</th>
+          <th>Check Out Date</th>
+          <th>Number of Nights</th>
+        </tr>
+        {filteredData.map((item) => (
+          <tr key={item.firstName}>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.firstName}</td>
+            <td>{item.surname}</td>
+            <td>{item.email}</td>
+            <td>{item.roomId}</td>
+            <td>{item.checkInDate}</td>
+            <td>{item.checkOutDate}</td>
+            <td>{item.NumberofNights}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
+
+export default SearchBar;
